03c9cabe10dcc49181acde5520b96939520bd74f
tcnecalle <carl.ahrsjo@thomascook.se>
2018-09-25 08:37:44 +0200

    Gifs are fun

--- src/RandomQuotes/RandomQuote.jsx
+++ src/RandomQuotes/RandomQuote.jsx
@@ -5,16 +5,35 @@ class RandomQuote extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { quoteToShow: {} };
+    this.state = { quoteToShow: {}, gifUrl: '' };
   }
 
   generateQuote = random => {
     this.setState({ quoteToShow: this.props.quotes.quotes[random] });
   };
 
+  generateGif = async () => {
+    const response = await fetch('https://www.reddit.com/r/reactiongifs.json');
+    const json = await response.json();
+
+    const index = Math.floor(Math.random() * 25);
+
+    console.log('# rand ', index);
+    console.log('# length ', json.data.children.length, json.data.children);
+
+    let url = json.data.children[index].data.url;
+    if (url.indexOf('.gifv') > -1) {
+      url = json.data.children[0].data.url;
+    }
+
+    this.setState({
+      gifUrl: url,
+    });
+  };
+
   render() {
     const { quotes } = this.props;
-    const { quoteToShow } = this.state;
+    const { quoteToShow, gifUrl } = this.state;
 
     return (
       <div className="quote">
@@ -29,6 +48,18 @@ class RandomQuote extends Component {
         >
           Generate quote
         </button>
+
+        <button
+          className="generateQuote"
+          onClick={() => {
+            this.generateGif();
+          }}
+        >
+          CLICK FOR GIF
+        </button>
+
+        {gifUrl && <img style={{ height: '200px' }} src={gifUrl} alt="WOOPS" />}
+
         <React.Fragment>
           <p className="quoteText">{quoteToShow.quote}</p>
           <p className="quoteBy">{quoteToShow.by}</p>
